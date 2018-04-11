74265c76c2a04046f0b1fad495a4129f915a06b5
managard <managard@gmail.com>
2018-04-10 20:57:29 -0500

    Updated Common
    Common now has Constructor and Get Methods.

--- src/main/java/Common.java
+++ src/main/java/Common.java
@@ -1,13 +1,131 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public final class Common {
-	public final int openFreezerDoorChange, openFridgeDoorChange, closeFreezerDoorChange, closeFridgeDoorChange, freezerCompressorChange, fridgeCompressorChange;
-	public Common(int openFreezerDoorChange, int openFridgeDoorChange, int closeFreezerDoorChange, int closeFridgeDoorChange, int freezerCompressorChange, int fridgeCompressorChange) {
-		// TODO Auto-generated constructor stub
-		this.openFreezerDoorChange = openFreezerDoorChange;
-		this.openFridgeDoorChange = openFridgeDoorChange;
-		this.closeFreezerDoorChange = closeFreezerDoorChange;
-		this.closeFridgeDoorChange = closeFridgeDoorChange;
-		this.freezerCompressorChange = freezerCompressorChange;
-		this.fridgeCompressorChange = fridgeCompressorChange;
+	private int[] info;
+	public Common() throws IOException {
+		fileImport();
+	}
+	public int getFridgeLow(){
+		return info[0];
+	}
+	public int getFridgeHigh() {
+		return info[1];
+	}
+	public int getFreezerLow() {
+		return info[2];
+	}
+	public int getFreezerHigh() {
+		return info[3];
+	}
+	public int getRoomLow() {
+		return info[4];
+	}
+	public int getRoomHigh() {
+		return info[5];
+	}
+	public int getFridgeRateLossDoorClosed() {
+		return info[6];
+	}
+	public int getFridgeRateLossDoorOpen() {
+		return info[7];
+	}
+	public int getFreezerRateLossDoorClosed() {
+		return info[8];
+	}
+	public int getFreezerRateLossDoorOpen() {
+		return info[9];
+	}
+	public int getFridgeCompressorStartDiff() {
+		return info[10];
+	}
+	public int getFreezerCompressorStartDiff() {
+		return info[11];
+	}
+	public int getFridgeCoolRate() {
+		return info[12];
+	}
+	public int getFreezerCoolRate() {
+		return info[13];
+	}
+	private void fileImport() throws IOException {
+		int input[] = new int[14];
+		String line;
+		try {
+			FileReader fr = new FileReader("config.ini");
+			BufferedReader br = new BufferedReader(fr); 
+			while((line=br.readLine())!=null) {
+				int temp;
+				int i;
+				for(i=0; line.charAt(i)!='='; i++) {
+				}
+				temp = whatInput(line.substring(0, i));
+				if(temp!=-1)
+					input[temp] =  Integer.parseInt(line.substring(i+1));
+			}
+			br.close();
+			fr.close();
+			info = input;
+		}
+        catch(FileNotFoundException ex) {
+            System.out.println("Unable to open file");                
+        }
+		catch(IOException ex) {
+			System.out.println("IOException");
+		}
+	}
+	private int whatInput(String test) {
+		int end;
+		switch(test) {
+		case "FridgeLow":
+			end = 0;
+			break;
+		case "FridgeHigh":
+			end = 1;
+			break;
+		case "FreezerLow":
+			end = 2;
+			break;
+		case "FreezerHigh":
+			end = 3;
+			break;
+		case "RoomLow":
+			end = 4;
+			break;
+		case "RoomHigh":
+			end = 5;
+			break;
+		case "FridgeRateLossDoorClosed":
+			end = 6;
+			break;
+		case "FridgeRateLossDoorOpen":
+			end = 7;
+			break;
+		case "FreezerRateLossDoorClosed":
+			end = 8;
+			break;
+		case "FreezerRateLossDoorOpen":
+			end = 9;
+			break;
+		case "FridgeCompressorStartDiff":
+			end = 10;
+			break;
+		case "FreezerCompressorStartDiff":
+			end = 11;
+			break;
+		case "FridgeCoolRate":
+			end = 12;
+			break;
+		case "FreezerCoolRate":
+			end = 13;
+			break;
+		default:
+			end = -1;	
+			break;
+		}
+		return end;
 	}
 }
